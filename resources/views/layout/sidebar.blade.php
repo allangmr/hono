86e6a369f7865aa840517a505bf1ad4fc8bebcb4
Allan Montilla <allangmr7@gmail.com>
2020-03-12 16:37:48 -0500

    Se quita clase active de sidebar

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -8,7 +8,7 @@
 				<li class="submenu">
 					<a href="#"><i class="la la-dashboard"></i> <span> Dashboard</span> <span class="menu-arrow"></span></a>
 					<ul style="display: none;">
-						<li><a class="active" href="index.html">Admin Dashboard</a></li>
+						<li><a href="index.html">Admin Dashboard</a></li>
 						<li><a href="employee-dashboard.html">Employee Dashboard</a></li>
 					</ul>
 				</li>
